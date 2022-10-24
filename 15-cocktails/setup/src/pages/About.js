d8d8561ccd9f8115df002aa7bcf41c1e68b81d6c
DenysSemanchuk1 <semanchukdenys@gmail.com>
2022-10-24 16:09:49 +0200

    added 15 projects

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam magni
+        delectus, eum, molestias nihil ut sapiente unde temporibus in neque
+        repellendus fugit ipsum eius pariatur facilis perspiciatis eos
+        accusantium suscipit tempora! Sit officiis eius sapiente, enim magni
+        vitae nesciunt. Necessitatibus!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
